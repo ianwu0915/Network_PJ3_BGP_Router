b303417f349788da8dba8a70a9eb61f74f0f9ed6
hanks <chenhanks@gmail.com>
2024-02-18 09:59:36 -0500

    adding toString and belong to

--- ipAddress.py
+++ ipAddress.py
@@ -3,39 +3,49 @@ class Ip:
     def __init__(self, ip, netmask):
         self.length = 4
         self.address = [int(qdn) for qdn in ip.split('.')]
-        self.binary_address = [int(format(part, "08b")) for part in self.address]
+        #self.binary_address = "".join(format(part, "08b") for part in self.address)
         self.mask = [int(qdn) for qdn in netmask.split('.')]
-        self.binary_mask = [int(format(part, "08b")) for part in self.mask]
-        self.network_prefix = [self.address[i] & self.mask[i] for i in range(len(self.address))]
-        self.binary_network = [int(format(part, "08b")) for part in self.network_prefix]
-
+        #self.binary_mask = "".join(format(part, "08b") for part in self.mask)
+        self.network_prefix = [self.address[i] & self.mask[i] for i in range(self.length)]
+        #self.binary_network = [int(format(part, "08b")) for part in self.network_prefix]
+        self.mask_len = 0
+        for i in range(self.length):
+            self.mask_len += self.mask[i].bit_count()
 
     def longest_match(self, other_ip):
         if isinstance(other_ip, Ip):
-            other_binary_address = other_ip.binary_network
+            other_network= other_ip.network_prefix
         else:
             other_address = [int(qdn) for qdn in other_ip.split('.')]
-            other_binary_address = [int(format(part, '08b')) for part in other_address]
-
         max_match_length = 0
         for i in range(self.length):
-            xor_result = self.binary_network[i] ^ other_binary_address[i]
+            xor_result = int(self.network_prefix[i]) ^ int(other_network[i])
             match_length = 0
             while xor_result > 0:
                 xor_result >>= 1
                 match_length += 1
             if match_length > max_match_length:
                 max_match_length = match_length
-
         return max_match_length
     
     def belong_to(self, other_ip) -> bool:
         if isinstance(other_ip, Ip):
-            
+            other_network = other_ip.network_prefix
+        else:
+            other_address = [int(qdn) for qdn in other_ip.split(".")]
+            other_network = [other_address[i] & self.mask[i] for i in range(self.length)]
+        for i in range(self.length):
+            if self.network_prefix[i] != other_network[i]:
+                return False
+        return True
+    
+    def __str__(self) -> str:
+        return ".".join(str(qdn) for qdn in self.address) + f"/{self.mask_len}"
+
+    
 if __name__ == "__main__":
     x = Ip("192.168.0.1", "255.255.255.0")
-    y = Ip("192.168.0.10", "255.255.255.0")
-    print(x.binary_network)
-    print(y.binary_network)
-    match_length = x.longest_match(y)
-    print("Longest match length:", match_length)
+    y = Ip("192.168.1.10", "255.255.254.0")
+    print(x.mask_len)
+    print(x)
+    print(x.belong_to(y))
